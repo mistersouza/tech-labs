2e8ad2b20613166402f99f08269b0a3800b9aba1
Mister Souza <mistersouza@icloud.com>
2024-11-12 15:59:51 +0100

    Init commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+// Full Documentation - https://docs.turbo360.co
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.render('index', { text: 'This is the dynamic data. Open index.js from the routes directory to see.' })
+})
+
+module.exports = router
