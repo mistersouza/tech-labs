b156d54568c37eca928e38d2dbd3a9a12fce657d
Mister Souza <mistersouza@icloud.com>
2024-11-12 21:51:57 +0100

    Get profile by id

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,27 @@
 const express = require('express')
 const router = express.Router()
 
+const profiles = [
+  {
+    id: 1,
+    name: 'John Doe',
+    age: 30,
+    email: 'john.doe@example.com'
+  },
+  {
+    id: 2,
+    name: 'Jane Doe',
+    age: 25,
+    email: 'jane.doe@example.com'
+  },
+  {
+    id: 3,
+    name: 'Bob Smith',
+    age: 35,
+    email: 'bob.smith@example.com'
+  }
+]
+
 router.get('/', (req, res) => {
   res.render('index', { text: 'This is the dynamic data. Open index.js from the routes directory to see.' })
 })
@@ -12,12 +33,19 @@ router.get('/query', (request, response) => {
   })
 })
 
-router.get('/:profileId/:username', (request, response) => {
-  const { profileId, username } = request.params
+router.get('/:id', (request, response) => {
+  const { id } = request.params
+
+  if (id >= profiles.length || id < 0) {
+    response.json({
+      confirmation: 'fail',
+      message: 'Profile does not exist'
+    })
+    return;
+  }
 
   response.render('profile', {
-    profileId,
-    username
+    profile: profiles[id - 1]
   })
 })
 
@@ -26,5 +54,7 @@ router.post('/post', (request, response) => {
     data: request.body
   })
 })
+
+
 
 module.exports = router
