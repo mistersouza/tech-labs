8269a84a17ff9b08b0b98fe97974c3e3a719b6bd
Thiago Souza <mistersouza@icloud.com>
2024-11-14 21:12:45 +0000

    Define and export Person schema and model

---
+++ models/Person.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const personSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+    },
+    favoriteFoods: {
+        type: [String],
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Person', personSchema);
--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 require('dotenv').config();
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Conneced to MongoDB'))
+  .catch((error) => console.log('Error connecting to DB', error))
 
-let Person;
+let Person = require('./models/Person'); 
 
-const createAndSavePerson = (done) => {
   done(null /*, data*/);
 };
 
